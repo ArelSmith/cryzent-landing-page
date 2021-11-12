0c5e67c0bdd832636f4c2349619f31914e019bbc
ArelSmith <wonderfulofgame@gmail.com>
2021-11-12 17:11:19 +0700

    Menambahkan beberapa perubahan

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
-$(document).ready(function() {
-    
-});
+const loader = document.querySelector('.loading-panel');
+
+function loadPage()
+{
+    const fEffect = setInterval(() => {
+        if(!loader.style.opacity) {
+            loader.style.opacity = 1;
+        }
+        if(loader.style.opacity > 0) {
+            loader.style.opacity -= 0.1;
+        } else {
+            clearInterval(fEffect);
+            loader.style.display = "none";
+        }
+    }, 100)
+}
